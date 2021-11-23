f8e8e1c6b3b69258962e82777bafff0134ff52f3
Ali Anwar <alianwar@live.com>
2021-11-23 14:28:08 +0500

    health endpoint added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
+
+
+
+function getHealth(req:express.Request, res:express.Response) 
+{
+  return res.status(200).send("Server is running...");
+}
+
+app.get('/', getHealth);
+
 app.listen(port, () => {
   console.log(`Express server is running on port ${port}.`);
 });
